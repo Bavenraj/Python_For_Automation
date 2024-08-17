8064ce7e5a984d35ec826529b94e89aff09c2fa3
BAVENRAJ <bavenraj08@gmail.com>
2024-08-18 00:08:49 +0800

    l

--- practice.py
+++ practice.py
@@ -1,4 +1,7 @@
 import subprocess
+from pathlib import Path
 
 for i in range(5):
-    subprocess.check_call(["py", "read_files.py"])
+    subprocess.check_call(["py", "write_files_starter_code.py"])
+    
+print(Path("read_files.py").suffix)
