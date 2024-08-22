e530d6bed7854fc0cd3ab192e3a47809f8b9bddf
BAVENRAJ <bavenraj08@gmail.com>
2024-08-22 10:23:24 +0800

    403

---
+++ web_scraping/bypass_403_status.py
@@ -0,0 +1,12 @@
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+url =  "https://www.brickz.my/"
+ua = UserAgent()
+#print(ua.chrome)
+header = {'User-Agent':f"{ua.chrome}"}
+response_with_user_agent = requests.get(url, headers=header)
+response_without_user_agent = requests.get(url, headers={"Accept": "text/html"})
+
+print(response_without_user_agent) #result in 403 forbidden status
+print(response_with_user_agent) #result in 200 status
