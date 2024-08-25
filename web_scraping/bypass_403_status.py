8a89c336172a52b0fc435c0b5f5a194707d435a1
BAVENRAJ <bavenraj08@gmail.com>
2024-08-25 22:23:45 +0800

    selenium

--- web_scraping/bypass_403_status.py
+++ web_scraping/bypass_403_status.py
@@ -7,6 +7,3 @@ ua = UserAgent()
 header = {'User-Agent':f"{ua.chrome}"}
 response_with_user_agent = requests.get(url, headers=header)
 response_without_user_agent = requests.get(url, headers={"Accept": "text/html"})
-
-print(response_without_user_agent) #result in 403 forbidden status
-print(response_with_user_agent) #result in 200 status
