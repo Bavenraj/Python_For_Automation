5e244355190504ea1ef391bb0889c44fb18e1bd8
BAVENRAJ <bavenraj08@gmail.com>
2024-08-27 22:53:00 +0800

    selenium

--- web_scraping/automate_form_filling.py
+++ web_scraping/automate_form_filling.py
@@ -13,7 +13,7 @@ driver.maximize_window()
 driver.get(url)
 
 first_name = driver.find_element(By.XPATH, '//*[@id="input-firstname"]')
-first_name.send_keys("John"
+first_name.send_keys("John")
 
 last_name = driver.find_element(By.XPATH, '//*[@id="input-lastname"]')
 last_name.send_keys("Doe")
